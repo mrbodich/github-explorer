91a2882e37d7f91f532120e144b8ee2534a77e0c
Bogdan <email@server.domen>
2023-03-25 08:39:30 +0200

    Initial Commit

---
+++ Github Explorer/Github_ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ExplorerApp.swift
+//  Github Explorer
+//
+//  Created by Bogdan Chornobryvets on 25.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Github_ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
